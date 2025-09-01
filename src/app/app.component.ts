d0e4bf027dc1c0c253cfef796affaa316a7d58df
edumaldo94 <128635985+edumaldo94@users.noreply.github.com>
2025-09-01 08:20:37 -0300

    toastr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TarjetaCreditoComponent } from './component/tarjeta-credito/tarjeta-credito.component';
+
 @Component({
   selector: 'app-root',
    standalone: true,
--- src/app/component/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/component/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,22 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-tarjeta-credito',
-  standalone: true, 
-  imports: [CommonModule], 
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './tarjeta-credito.component.html',
-  styleUrls: ['./tarjeta-credito.component.scss'] 
+  styleUrls: ['./tarjeta-credito.component.scss']
 })
-export class TarjetaCreditoComponent  {
+export class TarjetaCreditoComponent implements OnInit {
 
-  listTarjetas: any[] = [ 
+  listTarjetas: any[] = [
     { titular: 'Eduardo', numero: '1234 5678 9012 3456', fechaExpiracion: '12/24', cvv: '123', tipo: 'Visa' },
     { titular: 'Maria', numero: '9876 5432 1098 7654', fechaExpiracion: '11/23', cvv: '456', tipo: 'MasterCard' },
     { titular: 'Juan', numero: '4567 8901 2345 6789', fechaExpiracion: '10/25', cvv: '789', tipo: 'American Express' }
   ];
 
-  constructor() { }
+  form:FormGroup;
 
-  ngOnInit(): void { } 
+  constructor( private fb: FormBuilder,private toastr: ToastrService) {
+    this.form = this.fb.group({
+      titular: ['',Validators.required],
+      numero: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+  fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+    });
+  }
+
+  ngOnInit(): void { }
+
+  agregarTarjeta(){
+    console.log(this.form);
+  const tarjeta: any = {
+    titular: this.form.value.titular,
+    numero: this.form.value.numero,
+    fechaExpiracion: this.form.value.fechaExpiracion,
+    cvv: this.form.value.cvv,
+
+  }
+  this.listTarjetas.push(tarjeta);
+  this.toastr.success('Tarjeta agregada con exito', 'Tarjeta Agregada', {
+    timeOut: 3000,
+    positionClass: 'toast-top-right'
+  });
+  this.form.reset();
+};
+deletCard(index:number){
+
+this.listTarjetas.splice(index,1);
+ this.toastr.error('Tarjeta eliminada con exito', 'Tarjeta Eliminada', {
+    timeOut: 3000,
+   positionClass: 'toast-top-right'
+});
+};
+
 }
